f1cc801a59f58a5f879f05dc7ce33ed037cd25da
arifoglu <88225121+arifoglu@users.noreply.github.com>
2023-10-24 15:13:38 +0200

    i did small page with jquery

---
+++ lesson1/script.js
@@ -0,0 +1,70 @@
+// $(document).ready(function(){
+//     alert("jquery loaded");
+// }); 
+
+// for select use $ sign
+
+let heading = $("#logo");
+//console.log(heading);
+
+let banner = $(".wrapper");
+// console.log(banner);
+
+////////////////////////////jquery selectors
+
+// $("#lead-banner p");
+// 
+// //tag select
+// $("h3").css({border:"3px solid blue"});
+// 
+// // class select
+// $(".wrapper").css({border:"3px solid red"});
+// 
+// // id select
+// $("#clients").css({border:"3px solid yellow"});
+
+
+////////////////////////////jquery filters
+
+// use : sign for filtering
+
+// $("header nav li:first").css({border:"2px solid red"});
+// 
+// $("header nav li:last").css({border:"2px solid red"});
+
+//// first - last
+// $("#contact ul li:first").css({border:"2px solid red"});
+// $("#contact ul li:last").css({border:"2px solid red"});
+
+/// even -odd
+// $("header nav li:even").css({border:"2px solid blue"});
+// $("header nav li:odd").css({border:"2px solid red"});
+
+/// not
+// $("section:not('#contact')").css({border:"2px solid green"});
+
+//////////////////////////// DOM
+
+// next()
+//$("#contact-methods").next().css({border:"2px solid red"});
+//
+//// prev()
+//$("#social-nav").prev().css({border:"2px solid blue"});
+//
+//// parent() parents()
+//$(".banner-title").parent().css({border:"2px solid blue"});
+//
+//// children()
+//$("#social-nav").children().css({border:"2px solid green"});
+//
+//// find()
+//$("#social-nav").find(".facebook").css({border:"2px solid blue"});
+
+///////////////////////////// chaining
+
+// $("#contact-methods").css({border:"3px solid red"})
+// .next().css({border:"3px solid green"})
+// .closest("section").css({border:"2px solid blue"});
+
+
+
